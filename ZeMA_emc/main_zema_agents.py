13b5d357df5abd1b223e2ddb948c10354a1ff094
YONG BANG XIANG <yongbangxiang@gmail.com>
2019-07-25 18:29:59 +0100

    updated handling multiple matplotlib plots

--- ZeMA_emc/main_zema_agents.py
+++ ZeMA_emc/main_zema_agents.py
@@ -17,7 +17,8 @@ if __name__ == '__main__':
     fft_bfc_agent = agentNetwork.add_agent(agentType=FFT_BFCAgent)
     pearson_fs_agent = agentNetwork.add_agent(agentType=Pearson_FeatureSelectionAgent)
     lda_agent = agentNetwork.add_agent(agentType=LDA_Agent)
-    regression_agent = agentNetwork.add_agent(agentType=Regression_Agent)
+    bayesianRidge_agent = agentNetwork.add_agent(agentType=Regression_Agent, name="BayesianRidge_Agent")
+    randomForest_agent = agentNetwork.add_agent(agentType=Regression_Agent, name="RandomForest_Agent")
     evaluator_agent = agentNetwork.add_agent(agentType=EvaluatorAgent)
     monitor_agent = agentNetwork.add_agent(agentType=MonitorAgent)
 
@@ -26,27 +27,32 @@ if __name__ == '__main__':
     train_test_split_agent.init_parameters(train_ratio=0.8)
     
     # init parameters for regression
-    regression_agent.init_parameters(regression_model="BayesianRidge")
-    #or
-    #regression_agent.init_parameters(regression_model="RandomForest")
+    bayesianRidge_agent.init_parameters(regression_model="BayesianRidge")
+    randomForest_agent.init_parameters(regression_model="RandomForest")
 
     #bind agents
     agentNetwork.bind_agents(datastream_agent, train_test_split_agent)
     agentNetwork.bind_agents(train_test_split_agent, fft_bfc_agent)
     agentNetwork.bind_agents(fft_bfc_agent, pearson_fs_agent)
+
     agentNetwork.bind_agents(pearson_fs_agent, lda_agent)
     agentNetwork.bind_agents(lda_agent, evaluator_agent)
-    # Maybe these two lines ???
-    agentNetwork.bind_agents(pearson_fs_agent, regression_agent)
-    agentNetwork.bind_agents(regression_agent, evaluator_agent)
-    
+
+    agentNetwork.bind_agents(pearson_fs_agent, bayesianRidge_agent)
+    agentNetwork.bind_agents(bayesianRidge_agent, evaluator_agent)
+
+    agentNetwork.bind_agents(pearson_fs_agent, randomForest_agent)
+    agentNetwork.bind_agents(randomForest_agent, evaluator_agent)
+
     #bind to monitor agents
     agentNetwork.bind_agents(fft_bfc_agent, monitor_agent)
     agentNetwork.bind_agents(pearson_fs_agent, monitor_agent)
-    agentNetwork.bind_agents(lda_agent, monitor_agent)
-    agentNetwork.bind_agents(regression_agent, monitor_agent)
-    agentNetwork.bind_agents(evaluator_agent, monitor_agent)
 
+    agentNetwork.bind_agents(lda_agent, monitor_agent)
+    agentNetwork.bind_agents(bayesianRidge_agent, monitor_agent)
+    agentNetwork.bind_agents(randomForest_agent, monitor_agent)
+
+    agentNetwork.bind_agents(evaluator_agent, monitor_agent)
 
     #trigger datastream to send all at once
     datastream_agent.send_all_sample()
